517d33d799a4019d2d5494bb44b45cf4b3a8366a
sadhin28 <taosifsadhin113@gmail.com>
2025-05-10 02:01:39 +0600

    done

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,16 @@ function App() {
 
   return (
     <div className='mx-5  md:text-white'>
-      <div className='sticky top-0 lg:bg-gray-600 bg-gray-600  '>
+     <div className=''>
+        <div className='sticky top-0 lg:bg-gray-600 bg-gray-600  '>
        {/* <Navbar></Navbar> */}
        <Mynavbar></Mynavbar>
        
     </div>
     < Priceoptions></Priceoptions>
-    <div className='grid justify-center gap-5 md:grid-cols-2 relative -z-30'>
+    
+     </div>
+     <div className='grid justify-center gap-5 md:grid-cols-2 relative -z-30'>
        <LineCharts></LineCharts>
        <BarCharts></BarCharts>
     </div>
