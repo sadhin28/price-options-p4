f9702c24efc5db3b75a47f4bb9d9b18bff994999
sadhin28 <taosifsadhin113@gmail.com>
2025-05-09 23:24:15 +0600

    add flex grow

--- src/components/PriceOption/PriceOption.jsx
+++ src/components/PriceOption/PriceOption.jsx
@@ -3,7 +3,7 @@ const PriceOption = ({option}) => {
      const {name,id,price,features}=option;
    
     return (
-        <div className='text-center text-white bg-blue-600 border rounded-2xl p-4 '>
+        <div className='text-center flex flex-col  text-white bg-blue-600 border rounded-2xl p-4 '>
             <h2 className="">
                  <span className='text-5xl'>{price}</span>
                  <span className='text-2xl'>/mon</span>
@@ -11,13 +11,17 @@ const PriceOption = ({option}) => {
             <h4 className='text-3xl'>{name}</h4>
             <div className="divider "></div>
         
-              {
-                  features.map(f=><p className="flex items-center gap-2"><MdSportsGymnastics />{f}</p>)
-              }
+             <div className="flex-grow">
+                 {
+                  features.map(f=><p className="flex flex-grow items-center gap-2"><MdSportsGymnastics />{f}</p>)
+                }
 
-              <div className="mt-5 mb-5">
+                
+             </div>
+
+              <div className="mt-5 mb-5 ">
                  <button className="btn bg-green-700 w-full border-none  text-2xl p-6 text-white rounded-4xl hover:bg-green-900">Buy Now</button>
-              </div>
+             </div>
            
         </div>
     );
